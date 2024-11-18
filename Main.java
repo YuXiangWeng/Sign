ef35747fb5a812df74a19fac27cbf295cc85edee
YuXiangWeng <yuxiangw4@nycstudents.net>
2024-11-18 16:47:59 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[]args){
+        Sign a = new Sign(null, 0);
+        System.out.println(a);
+    }
+}
---
+++ Sign.java
@@ -0,0 +1,6 @@
+public class Sign{
+    public Sign (String message,int Width ){
+        String result = "";
+        for (int i = 0; i <)
+    }
+}
